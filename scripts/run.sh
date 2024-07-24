4df30b67cdbfe55437f3e38f5e8cce8f6b9ef53c
ItzTas <talessabini2@gmail.com>
2024-07-23 22:26:29 -0300

    tests with druid library

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,17 @@ cd todo-rust || printf ""
 
 cargo build
 
+code=$?
+
+if [ "$code" -ne 0 ]; then
+	echo "could not compile exit: $code."
+	exit 1
+fi
+
+echo "---Compiled!---"
+echo ""
+
 ./target/debug/todo-rust
+
+echo ""
+echo ""
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use druid::{
+    widget::{Button, Flex, Scroll},
+    AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc,
+};
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(build_widget());
+    let data = 0_u32;
+    AppLauncher::with_window(main_window)
+        .log_to_console()
+        .launch(data)
+}
+
+fn build_widget() -> impl Widget<u32> {
+    let mut col = Flex::column();
+    for i in 0..30 {
+        let button = Button::new(format!("Button {}", i)).padding(5.);
+        col.add_child(button)
+    }
+    Scroll::new(col)
 }
