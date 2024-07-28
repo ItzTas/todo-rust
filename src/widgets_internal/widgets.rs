f8792ee69d0bd8d72bfd6c060958e46529a910d6
ItzTas <talessabini2@gmail.com>
2024-07-28 01:45:27 -0300

    todo implemented

--- src/widgets_internal/widgets.rs
+++ src/widgets_internal/widgets.rs
@@ -1,6 +1,6 @@
 use druid::{
-    widget::{Flex, Label, TextBox},
-    LensExt, Widget, WidgetExt,
+    widget::{Button, Flex, Label, TextBox},
+    LensExt, LocalizedString, Widget, WidgetExt,
 };
 
 use crate::models::{app_state::AppState, widget_data::InputData};
@@ -11,15 +11,32 @@ pub fn build_main_widget() -> impl Widget<AppState> {
         .padding(5.)
         .lens(AppState::input_data.then(InputData::input_title));
     let desc_input = TextBox::new()
-        .with_placeholder("Dog")
+        .with_placeholder("Walk the dog")
         .padding(5.)
         .lens(AppState::input_data.then(InputData::input_text));
+
+    let text: LocalizedString<AppState> = LocalizedString::new("add task");
+
+    let add_button = Button::new(text.clone()).on_click(|_ctx, data: &mut AppState, _env| {
+        data.task_data.task_title = data.input_data.input_title.clone();
+        data.task_data.task_text = data.input_data.input_text.clone();
+    });
+
+    let task_title_label =
+        Label::new(|data: &AppState, _env: &_| format!("Title: {}", data.task_data.task_title))
+            .padding(5.);
+    let task_desc_label =
+        Label::new(|data: &AppState, _env: &_| format!("Desc: {}", data.task_data.task_text))
+            .padding(5.);
 
     Flex::column()
         .with_child(build_label(5., "insert title"))
         .with_child(title_input)
         .with_child(build_label(5., "insert description"))
         .with_child(desc_input)
+        .with_child(add_button)
+        .with_child(task_title_label)
+        .with_child(task_desc_label)
 }
 
 pub fn build_label(pad: f64, label_text: &str) -> impl Widget<AppState> {
